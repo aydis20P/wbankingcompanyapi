3c0726dc2cf289d7d9418e48853c330c71f0be60
williampalacios <mich.palacios.hdz@gmail.com>
2021-05-29 18:05:31 -0500

    commit inicial

---
+++ application/controllers/doc/api_data.js
@@ -0,0 +1,99 @@
+define({ "api": [
+  {
+    "success": {
+      "fields": {
+        "Success 200": [
+          {
+            "group": "Success 200",
+            "optional": false,
+            "field": "varname1",
+            "description": "<p>No type.</p>"
+          },
+          {
+            "group": "Success 200",
+            "type": "String",
+            "optional": false,
+            "field": "varname2",
+            "description": "<p>With type.</p>"
+          }
+        ]
+      }
+    },
+    "type": "",
+    "url": "",
+    "version": "0.0.0",
+    "filename": "./doc/main.js",
+    "group": "/Users/williampalacios/Development/wbankingcompaniapi/application/controllers/doc/main.js",
+    "groupTitle": "/Users/williampalacios/Development/wbankingcompaniapi/application/controllers/doc/main.js",
+    "name": ""
+  },
+  {
+    "type": "get",
+    "url": "/user/:id",
+    "title": "Request User information",
+    "version": "0.1.0",
+    "name": "GetUser",
+    "group": "User",
+    "parameter": {
+      "fields": {
+        "Parameter": [
+          {
+            "group": "Parameter",
+            "type": "Number",
+            "optional": false,
+            "field": "id",
+            "description": "<p>Users unique ID.</p>"
+          }
+        ]
+      }
+    },
+    "success": {
+      "fields": {
+        "Success 200": [
+          {
+            "group": "Success 200",
+            "type": "String",
+            "optional": false,
+            "field": "firstname",
+            "description": "<p>Firstname of the User.</p>"
+          },
+          {
+            "group": "Success 200",
+            "type": "String",
+            "optional": false,
+            "field": "lastname",
+            "description": "<p>Lastname of the User.</p>"
+          }
+        ]
+      },
+      "examples": [
+        {
+          "title": "Success-Response:",
+          "content": "HTTP/1.1 200 OK\n{\n  \"firstname\": \"John\",\n  \"lastname\": \"Doe\"\n}",
+          "type": "json"
+        }
+      ]
+    },
+    "error": {
+      "fields": {
+        "Error 4xx": [
+          {
+            "group": "Error 4xx",
+            "optional": false,
+            "field": "UserNotFound",
+            "description": "<p>The id of the User was not found.</p>"
+          }
+        ]
+      },
+      "examples": [
+        {
+          "title": "Error-Response:",
+          "content": "HTTP/1.1 404 Not Found\n{\n  \"error\": \"UserNotFound\"\n}",
+          "type": "json"
+        }
+      ]
+    },
+    "filename": "./Welcome.php",
+    "groupTitle": "User"
+  }
+] });
---
+++ application/controllers/doc/api_project.js
@@ -0,0 +1,16 @@
+define({
+  "name": "W Banking Company",
+  "version": "0.1.0",
+  "description": "Api de la W Banking Company",
+  "title": "API | W Banking Company",
+  "url": "https://wbankingcompany.herokuapp.com/v1",
+  "sampleUrl": false,
+  "defaultVersion": "0.0.0",
+  "apidoc": "0.3.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2021-05-29T23:00:51.735Z",
+    "url": "https://apidocjs.com",
+    "version": "0.28.1"
+  }
+});
