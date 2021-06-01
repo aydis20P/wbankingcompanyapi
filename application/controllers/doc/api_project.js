427de976e0205df4a175f7093c26ee9e13170a29
williampalacios <mich.palacios.hdz@gmail.com>
2021-05-31 20:01:50 -0500

    sampleUrl agregada a apiDoc

--- application/controllers/doc/api_project.js
+++ application/controllers/doc/api_project.js
@@ -4,12 +4,12 @@ define({
   "description": "Api de la W Banking Company",
   "title": "API | W Banking Company",
   "url": "https://wbankingcompany.herokuapp.com/index.php/v1",
-  "sampleUrl": false,
+  "sampleUrl": "https://wbankingcompany.herokuapp.com/index.php/v1",
   "defaultVersion": "0.0.0",
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2021-06-01T00:35:07.259Z",
+    "time": "2021-06-01T00:53:35.613Z",
     "url": "https://apidocjs.com",
     "version": "0.28.1"
   }
