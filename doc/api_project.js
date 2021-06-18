87f8bf25f7038f977cf2a0c30a92e58b2927c5de
williampalacios <mich.palacios.hdz@gmail.com>
2021-06-18 02:45:58 -0500

    API documentada con auth v1

--- doc/api_project.js
+++ doc/api_project.js
@@ -9,7 +9,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2021-06-18T02:42:28.956Z",
+    "time": "2021-06-18T07:42:37.916Z",
     "url": "https://apidocjs.com",
     "version": "0.28.1"
   }
